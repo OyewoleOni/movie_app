7aaef8250adee996eb76b02fc1eb1e0d8a73e6f2
Oyewole Oni <ooni@eminenttechnology.com>
2021-01-23 09:44:40 +0100

    Add ListView.Builder

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,11 +1,63 @@
-
 import 'package:flutter/material.dart';
+import 'package:movie_app/ui/details.dart';
+
+// import 'details.dart';
+
 
 class MovieListView extends StatelessWidget {
+  final List movies = [
+    "Titanic",
+    "Blade Runner",
+    "Rambo",
+    "The Avengers",
+    "Avatar",
+    "I Am Legend",
+    "300",
+    "The Wolf of Wall Street",
+    "Game of Thrones",
+    "Vikings",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      backgroundColor: Colors.blueGrey.shade400,
+      body: ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (
+            BuildContext context,
+            int index,
+          ) {
+            return Card(
+              elevation: 10,
+              color: Colors.white,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Container(
+                    // decoration: BoxDecoration(
+                    //   color: Colors.redAccent,
+                    //   borderRadius: BorderRadius.circular(12)
+                    // ),
+                    child: Text("M"),
+                  ),
+                ),
+                trailing: Text("..."),
+                title: Text(
+                  movies.elementAt(index),
+                ),
+                subtitle: Text("sub"),
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MovieListViewDetail()));
+                },
+                //onTap: () => debugPrint("Movie name: ${movies.elementAt(index)}"),
+              ),
+            );
+          }),
     );
   }
 }
+
