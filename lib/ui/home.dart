988bd72200facd4e7ef1194c82b41bc948452991
Oyewole Oni <ooni@eminenttechnology.com>
2021-01-23 17:30:06 +0100

    Building Custom Image widget

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -106,7 +106,32 @@ class MovieListView extends StatelessWidget {
               ),
             )),
       ),
-      onTap: () => debugPrint(movie.title),
+      onTap: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                MovieListViewDetail(movieName: movie.title, movie: movie),
+          ),
+        ),
+      },
+    );
+  }
+
+  Widget movieImage(String imageUrl) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: NetworkImage(
+            imageUrl ??
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0NzAxODAyMl5BMl5BanBnXkFtZTgwMDg0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg',
+          ),
+          fit: BoxFit.cover
+        ),
+      ),
     );
   }
 }
